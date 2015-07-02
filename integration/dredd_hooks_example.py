7196f695e01b38d2cf77c378960dac4f1ae9e6a1
w-vi <vilibald@wvi.cz>
2015-07-02 14:43:28 +0200

    Integration test with dredd

--- integration/dredd_hooks_example.py
+++ integration/dredd_hooks_example.py
@@ -3,7 +3,8 @@
 #  Copyright (c) 2015 Vilibald Wanča
 #  License: MIT
 #
-import dreddhooks as dredd
+import dredd_hooks as dredd
+import sys
 
 # *_all hooks
 @dredd.before_all
@@ -12,13 +13,19 @@ def before_all_test(transactions):
         transactions[0]['hooks_modifications'] = []
     transactions[0]['hooks_modifications'].append("python before all mod")
     print('python before all hook')
+    sys.stdout.flush()
+
 
 @dredd.after_all
 def after_all_test(transactions):
+    print('YAY', file=sys.stderr)
+    sys.stderr.flush()
     if 'hooks_modifications' not in transactions[0]:
         transactions[0]['hooks_modifications'] = []
     transactions[0]['hooks_modifications'].append("python after all mod")
     print('python after all hook')
+    sys.stdout.flush()
+
 
 # *_each hooks
 @dredd.before_each
@@ -27,6 +34,8 @@ def before_each_test(transaction):
         transaction['hooks_modifications'] = []
     transaction['hooks_modifications'].append("python before each mod")
     print('python before each hook')
+    sys.stdout.flush()
+
 
 @dredd.before_each_validation
 def before_each_validation_test(transaction):
@@ -34,6 +43,8 @@ def before_each_validation_test(transaction):
         transaction['hooks_modifications'] = []
     transaction['hooks_modifications'].append("python before each validation mod")
     print('python before each validation hook')
+    sys.stdout.flush()
+
 
 @dredd.after_each
 def after_each_test(transaction):
@@ -41,6 +52,8 @@ def after_each_test(transaction):
         transaction['hooks_modifications'] = []
     transaction['hooks_modifications'].append("python after each mod")
     print('python after each hook')
+    sys.stdout.flush()
+
 
 # *_each hooks
 @dredd.before_validation('Machines > Machines collection > Get Machines')
@@ -49,6 +62,8 @@ def before_validation_test(transaction):
         transaction['hooks_modifications'] = []
     transaction['hooks_modifications'].append("python before validation mod")
     print('python before validation hook')
+    sys.stdout.flush()
+
 
 @dredd.before("Machines > Machines collection > Get Machines")
 def before_test(transaction):
@@ -56,12 +71,14 @@ def before_test(transaction):
         transaction['hooks_modifications'] = []
     transaction['hooks_modifications'].append("python before mod")
     print('python before hook')
+    sys.stdout.flush()
+
 
 @dredd.after('Machines > Machines collection > Get Machines')
 def after_test(transaction):
     if 'hooks_modifications' not in transaction:
         transaction['hooks_modifications'] = []
     transaction['hooks_modifications'].append("python after mod")
-    print('python after hook')
     transaction['fail'] = 'Yay! Failed in python!'
-
+    print('python after hook')
+    sys.stdout.flush()
